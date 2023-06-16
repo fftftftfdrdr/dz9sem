2cbf0ae21a8eec051b5cc4ddc9a89bff6109cc35
Ivan Slukin <provaider010@gmail.com>
2023-06-16 19:59:48 +1000

    sds

---
+++ DZ_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+int SummaRec(int num1, int num2)
+{
+    if (num1 == num2) return num1;
+else Console.Write($"{SummaRec(num1, num2 + 1)}, ");
+return num2;
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите целое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SummaRec(num1, 1));
---
+++ DZ_2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SummaRec(int num, int num2)
+{
+    if (num>num2) return (0);
+    return num + SummaRec(num+1, num2);
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите целое число M ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите целое число N ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма рекурсией всех чисел равна {SummaRec(num, num2)}");
